38c44a232c067b6b7ca9b4fbc587ac38ea90d932
Rory Duncan <robust.rory@gmail.com>
2013-10-29 18:42:34 -0700

    Added painting.

--- example/js/HTML5-CPE.js
+++ example/js/HTML5-CPE.js
@@ -1,14 +1,44 @@
-var CanvasPixelEditor = function(appendToEl, options) {
-/* CONSTANTS */
-  var defaultPixelSize = 10;
+var CanvasPixelEditor = function(options) {
+
 /* PRIVATE */
 
   var CanvasEditor = function(canvas, pixelSize) {
 
-    console.log("subclass:", this);
-
-    this.penSize = pixelSize;
+    this.canvas = canvas;
+    this.pixel = pixelSize;
     this.context = canvas.getContext('2d');
+    this.clear = function() {
+      this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+     };
+    this.fill = function(color) {
+      var color = !color ? "#fff" : color; 
+      this.context.fillStyle = color;
+      this.context.fillRect(0,0, this.canvas.width, this.canvas.height);
+      return this;
+    };
+    this.currentColor = "#088";
+    this.dimensions = {
+      height: this.canvas.height/pixelSize,
+      width: this.canvas.width/pixelSize
+    }
+    this.paint = function(x, y) {
+      var pixel = this.pixelAt(x, y);
+      this.context.fillStyle = this.currentColor;
+
+      this.context.fillRect(pixel.x, pixel.y, this.pixel, this.pixel );
+
+    }
+    this.pixelAt = function(_x, _y) {
+      var x = ~~(_x / this.pixel),
+          y = ~~(_y / this.pixel),
+          xStart = x*this.pixel,
+          yStart = y*this.pixel,
+          xEnd = xStart + this.pixel,
+          yEnd = yStart + this.pixel;
+        return {x: xStart, y: yStart};
+
+     };
+
 
   };
 
@@ -22,6 +52,41 @@ var CanvasPixelEditor = function(appendToEl, options) {
     else return document.getElementById(Id);
   };
 
+/* Page variables and mouse function from http://stackoverflow.com/questions/1114465/getting-mouse-location-in-canvas*/
+
+  var getMouse = function(e, canvas) {
+
+    stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
+    stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
+    styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
+    styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
+    // Some pages have fixed-position bars (like the stumbleupon bar) at the top or left of the page
+    // They will mess up mouse coordinates and this fixes that
+    var html = document.body.parentNode;
+    htmlTop = html.offsetTop;
+    htmlLeft = html.offsetLeft;
+    var element = canvas, offsetX = 0, offsetY = 0, mx, my;
+
+    // Compute the total offset. It's possible to cache this if you want
+    if (element.offsetParent !== undefined) {
+      do {
+        offsetX += element.offsetLeft;
+        offsetY += element.offsetTop;
+      } while ((element = element.offsetParent));
+    }
+
+    // Add padding and border style widths to offset
+    // Also add the <html> offsets in case there's a position:fixed bar (like the stumbleupon bar)
+    // This part is not strictly necessary, it depends on your styling
+    offsetX += stylePaddingLeft + styleBorderLeft + htmlLeft;
+    offsetY += stylePaddingTop + styleBorderTop + htmlTop;
+
+    mx = e.pageX - offsetX;
+    my = e.pageY - offsetY;
+
+    // We return a simple javascript object with x and y defined
+    return {x: mx, y: my};
+  };
 
 /* PUBLIC */
   this.canvas = null;
@@ -36,7 +101,7 @@ var CanvasPixelEditor = function(appendToEl, options) {
     this.canvas = getCanvasById(Id);
     return this;
    };
-   this.getSize = function() {
+  this.getSize = function() {
     var coords = {};
     coords.width = this.canvas.width;
     coords.height = this.canvas.height;
@@ -59,7 +124,7 @@ var CanvasPixelEditor = function(appendToEl, options) {
   this.addCanvasToDOM = function(parentEl, options) {
     // the parent element that will be
     var parent = document.querySelectorAll(parentEl),
-        canv=document.createElement("canvas");
+        canv = document.createElement("canvas");
         
 
     if (parent.length === 0) { throw new Error("Specified Parent Element Not found in DOM.");}
@@ -84,33 +149,46 @@ var CanvasPixelEditor = function(appendToEl, options) {
   this.start = function(width, height, pixelSize) {
     var canvas = document.getElementById( this.getId() );
     if (!canvas) {throw new Error("Canvas was not found."); return;}
+    // fix from stack overflow for firefox for mouse captures
+    canvas.style.position = "relative";
     var pixelSize = !pixelSize ? pixelSize = defaultPixelSize : pixelSize;
     this.setSize({
       width: width*pixelSize,
       height: height*pixelSize
     });
-
-    this._core_ = new CanvasEditor(canvas, pixelSize);
+    // edit to make usage a different experience. 
+    this.edit = new CanvasEditor(canvas, pixelSize);
+    this._addEvents();
   
 
     return this;
    };
+  this._addEvents = function() {
+    // events are called in a different scope
+    var that = this; 
+    this.canvas.addEventListener("click", function(e) {
+
+      var mouse = getMouse(e, that.canvas);
+      that.edit.paint(mouse.x, mouse.y)
+    });
+   };
+  this._removeEvents = function() {
+    this.canvas.removeEventListener("click");
+   };
   this.end = function() {};
   this.save = function() {};
-  this.clear = function() {};
+  this.reset = function() {};
 
 
-// allow  canvas to be appended with instantiation.
-  if (appendToEl !== undefined) {
-    if (options !== undefined) {
-      this.addCanvasToDOM(appendToEl, options);
+// allows the canvas to be appended to another element upon instantiation.
+  if (options !== undefined) {
+    this.addCanvasToDOM(options.parent, options);
+    this.options = options;
     }
-    else this.addCanvasToDOM(appendToEl);
+  else {
+    this.addCanvasToDOM('body');
   }
 
 };
 
 
-window.CanvasPixelEditor = new CanvasPixelEditor('div.container', {id:"pixel-maker"});
-CanvasPixelEditor.start(25, 25, 10);
-console.log(CanvasPixelEditor);
--- example/js/main.js
+++ example/js/main.js
@@ -1,1 +1,6 @@
 
+
+
+CanvasPixelEditor = new CanvasPixelEditor({parent: 'div.container', id:"pixel-maker"});
+
+CanvasPixelEditor.start(10, 10, 40);
